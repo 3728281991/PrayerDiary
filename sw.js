5091d82c66b194b95db8ec1a08abbab3db1c948d
3728281991 <bodnarm595@gmail.com>
2025-08-22 18:43:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
 const CACHE_NAME = 'prayer-diary-v1.0';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/start-display.html',
-  '/download.html',
-  '/styles.css',
-  '/app.js',
+  'index.html',
+  'start-display.html',
+  'download.html',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;900&display=swap'
 ];
@@ -14,6 +12,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
